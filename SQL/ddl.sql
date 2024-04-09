48018862f2929b0ad448b075a45cd23503f5ea9a
Thanos Jia <147551437+ThanosJia@users.noreply.github.com>
2024-04-09 18:37:56 -0400

    updates to ddl file

--- SQL/ddl.sql
+++ SQL/ddl.sql
@@ -1,43 +1,82 @@
-CREATE TABLE users {
-    id SERIAL PRIMARY KEY,
-    account_type TEXT NOT NULL,
-    membership_status,
-    payment_method TEXT NOT NULL,
+CREATE TABLE members (
+    member_id SERIAL PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    user_name TEXT NOT NULL,
+    user_name TEXT NOT NULL UNIQUE,
+    pwd TEXT NOT NULL,
+    membership_status BOOLEAN,
+    payment_method TEXT NOT NULL
+);
+
+CREATE TABLE trainers (
+    trainer_id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    user_name TEXT NOT NULL UNIQUE,
+    pwd TEXT NOT NULL,
+    available_times TEXT NOT NULL
+);
+
+CREATE TABLE admins (
+    admin_id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    user_name TEXT NOT NULL UNIQUE,
     pwd TEXT NOT NULL
-};
+);
 
-CREATE TABLE training_sessions {
+CREATE TABLE training_sessions (
     session_id SERIAL PRIMARY KEY,
-    trainer_id,
-    member_id,
-    session_date DATE,
-    session_time TIME
-};
+    trainer_id INT,
+    member_id INT,
+    session_date DATE NOT NULL,
+    session_time TIME NOT NULL,
+    FOREIGN KEY (trainer_id) REFERENCES trainers(trainer_id),
+    FOREIGN KEY (member_id) REFERENCES members(member_id)
+);
 
-CREATE TABLE rooms {
+CREATE TABLE rooms (
     room_id SERIAL PRIMARY KEY,
-};
+    room_name TEXT NOT NULL,
+    room_capacity INT NOT NULL
+);
 
-CREATE TABLE room_bookings {
+CREATE TABLE room_bookings (
     booking_id SERIAL PRIMARY KEY,
-    room_id,
-    booking_date DATE,
-    booking_time TIME
-};
+    room_id INT,
+    booking_date DATE NOT NULL,
+    booking_time TIME NOT NULL,
+    FOREIGN KEY (room_id) REFERENCES rooms(room_id)
+);
 
-CREATE TABLE equipment {
+CREATE TABLE equipment (
     equipment_id SERIAL PRIMARY KEY,
     equipment_name TEXT NOT NULL,
-    equipment_status
-};
+    equipment_status TEXT NOT NULL
+);
 
-CREATE TABLE fitness_classes {
+CREATE TABLE fitness_classes (
     class_id SERIAL PRIMARY KEY,
     class_name TEXT NOT NULL,
-    class_room,
-    class_date DATE,
-    class_time TIME,
-};
+    class_room INT,
+    class_date DATE NOT NULL,
+    class_time TIME NOT NULL,
+    FOREIGN KEY (class_room) REFERENCES rooms(room_id)
+);
+
+CREATE TABLE fitness_goals (
+    goal_id SERIAL PRIMARY KEY,
+    member_id INT,
+    goal_type TEXT NOT NULL,
+    goal_value INT NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES members(member_id)
+);
+
+CREATE TABLE health_metrics (
+    metric_id SERIAL PRIMARY KEY,
+    member_id INT,
+    metric_type TEXT NOT NULL,
+    metric_value INT NOT NULL,
+    recorded_date DATE NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES members(member_id)
+);
